5a0a905b6d45ff08e370f999a3c4b6a5575effc8
joel fleuren <joelfleuren@wcw-stud-145-109-17-236.wireless.uva.nl>
2016-11-16 10:43:32 +0100

    api

--- joelfleuren-pset3/ViewController.swift
+++ joelfleuren-pset3/ViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    @IBOutlet weak var search: UISearchBar!
+    @IBOutlet weak var table: UITableView!
+    var movies: movies!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        getDataFromUrl(url: NSURL(string: "https://www.omdbapi.com/?t=Harry+Potter&y=&plot=short&r=json") as! URL) {
+        (data, response, error) in
+             guard let data = data, error == nil else { return }
+             guard let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200 else{ return }
+                do
+                {
+                    if let dict = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                        self.movies.Title = dict["Title"] as! String
+                        self.movies.Year = dict["Year"] as! String
+                        self.movies.Director = dict["Director"] as! String
+                        self.movies.Actors = dict["Actors"] as! String
+                        self.movies.Plot = dict["Plot"] as! String
+                    }
+                }catch{
+                    print("no succes")
+            }
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
+        URLSession.shared.dataTask(with: url ) {
+            (data, response, error) in
+            completion(data, response, error)
+            }.resume()
     }
+    func downloadImage(url: URL) {
+        print("Download Started")
+        getDataFromUrl(url: url) { (data, response, error)  in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() { () -> Void in
 
+            }
+        }
+    }
 
 }
-
---
+++ joelfleuren-pset3/movies.swift
@@ -0,0 +1,18 @@
+//
+//  movies.swift
+//  joelfleuren-pset3
+//
+//  Created by joel fleuren on 15-11-16.
+//  Copyright © 2016 joel fleuren. All rights reserved.
+//
+
+import Foundation
+class movies{
+    var Title = ""
+    var Image = ""
+    var Rating = ""
+    var Year = ""
+    var Director = ""
+    var Actors = ""
+    var Plot = ""
+}
